f32e8202b9a250963ab0f1670b3a884599c328e2
saravanan <saravanan@sellinall.com>
2023-01-24 12:27:54 +0530

    Test Lambda func

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -1,13 +1,29 @@
 package com.test;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App implements RequestHandler<Map<String,String>, String>
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        // process event
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass());
+        return "200 ok";
     }
 }
